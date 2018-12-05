d20871654adef59f3b14ba1a57d7ef1de427b58b
Sergey Redyuk <sergred@gmail.com>
2018-12-05 13:51:54 +0100

    First try on testing

--- evaluation.py
+++ evaluation.py
@@ -3,9 +3,63 @@
 
 """"""
 
+from sklearn.model_selection import train_test_split as split
+from quilt.data.usr import credit
+from tabulate import tabulate
+import pandas as pd
+import numpy as np
 import unittest
 
-from pipelines import CreditGPipeline
+from models import DecisionTree, RandomForest, ExtremelyRandomizedTrees, XGB
+from error_generation import ImplicitMissingValues, ExplicitMissingValues
+from profilers import SklearnPipelineProfiler, ErrorType, Severity
+from pipelines import WineQualityPipeline, WineQualityMissingPipeline
+from pipelines import AbalonePipeline, CreditGPipeline
+from test_suite import AutomatedTestSuite, TestSuite, Test, Warning
+from error_generation import Anomalies, Typos
+
+
+class Table:
+    def __init__(self, rows, columns, subrows, subcolumns):
+        self.row_header_len, self.col_header_len = 2, 2
+        self.rows_len, self.cols_len = len(rows), len(columns)
+        self.subrows_len, self.subcols_len = len(subrows) + 1, len(subcolumns)
+        self.dataset_baseline_title = 'baseline'
+        self.table = np.zeros(
+            (self.rows_len*self.subrows_len+self.row_header_len,
+             self.cols_len*self.subcols_len+self.col_header_len), dtype=object)
+        self.table[:self.col_header_len, :self.row_header_len] = np.array(
+            [['', 'classifiers'], ['datasets', 'tests\\anomalies']])
+        self.table[0, self.row_header_len:] = np.array(
+            [list(columns)[i//self.subcols_len]
+             for i in range(self.cols_len*self.subcols_len)])
+        self.table[1, self.row_header_len:] = np.array(
+            list(subcolumns)*self.cols_len)
+        self.table[self.col_header_len:, 0] = np.array(
+            [list(rows)[i//self.subrows_len]
+             if i % self.subrows_len == 0 else ''
+             for i in range(self.rows_len*self.subrows_len)])
+        self.table[self.col_header_len:, 1] = np.array(
+            ([self.dataset_baseline_title] + list(subrows))*self.rows_len)
+
+    def update(self, data, i, j, x=-1, y=-1):
+        if x == -1 and y == -1:
+            z = self.row_header_len + self.subcols_len*j
+            self.table[self.col_header_len + self.subrows_len*i,
+                       z:z+self.subcols_len] = data
+        elif x != -1 and y != -1:
+            self.table[self.col_header_len + self.subrows_len*i + x + 1,
+                       self.row_header_len + self.subcols_len*j + y] = data
+        else:
+            raise Exception('wrong subs')
+
+    def save(self, filename):
+        np.savetxt(filename, self.table, delimiter=",", fmt="%s")
+
+    def show(self):
+        res = tabulate(self.table, tablefmt='psql')
+        print(res)
+        return res
 
 
 class EvaluationSuite(unittest.TestSuite):
@@ -24,12 +78,69 @@ class EvaluationSuite(unittest.TestSuite):
 class CreditGTest(unittest.TestCase):
     def setUp(self):
         self.pipeline = CreditGPipeline()
+        data = credit.dataset_31_credit_g()
+        target = "class"
+        # I guess it will work only if the target value is the last one.
+        self.features = [col for col in data.columns if col != target]
+        X = data[self.features]
+        y = data[target]
+        sets = split(X, y, test_size=0.2, random_state=0)
+        self.X_train, self.X_test, self.y_train, self.y_test = sets
 
     def tearDown(self):
         pass
 
-    def test_(self):
-        pass
+    def test_missing_values_in_data_with_random_forest(self):
+        classifier = RandomForest()
+        model = self.pipeline.with_estimator(classifier).fit(self.X_train,
+                                                             self.y_train)
+        error_generator = ExplicitMissingValues()
+        num_cols = 3
+        # Will fail if the number of columns is less than 3
+        columns = np.random.choice(self.features, num_cols, replace=False)
+        corrupted_X_test = error_generator.run(self.X_test, columns=columns)
+        # prediction = model.predict(X_test)
+        # print(accuracy_score(y_test, prediction))
+
+        # suite = TestSuite()
+        automated_suite = AutomatedTestSuite()
+        pipeline_profile = SklearnPipelineProfiler().on(model)
+        tests, warnings = automated_suite.run(corrupted_X_test,
+                                              pipeline_profile)
+        for column in columns:
+            self.assertIn(Test(Severity.CRITICAL).is_complete(column), tests)
+            self.assertIn(Warning(ErrorType.MISSING_VALUE,
+                                  Severity.CRITICAL, """
+                                  Column %s is not complete""" % column),
+                          warnings)
+
+    def test_typos_in_data_with_random_forest(self):
+        classifier = RandomForest()
+        model = self.pipeline.with_estimator(classifier).fit(self.X_train,
+                                                             self.y_train)
+        error_generator = Typos()
+        num_cols = 3
+        # Will fail if the number of columns is less than 3
+        columns = np.random.choice(self.features, num_cols, replace=False)
+        corrupted_X_test = error_generator.run(self.X_test, columns=columns)
+        # prediction = model.predict(X_test)
+        # print(accuracy_score(y_test, prediction))
+
+        # suite = TestSuite()
+        automated_suite = AutomatedTestSuite()
+        pipeline_profile = SklearnPipelineProfiler().on(model)
+        tests, warnings = automated_suite.run(corrupted_X_test,
+                                              pipeline_profile)
+        for column in columns:
+            self.assertIn(Test(Severity.CRITICAL).is_in_range(column), tests)
+            self.assertIn(Warning(ErrorType.TYPO,
+                                  Severity.CRITICAL, """
+                                  Column %s is not complete""" % column),
+                          warnings)
+
+    # def test_(self):
+    #     pass
+
 
 # class Test(unittest.TestCase):
 #     def setUp(self):
--- test_suite.py
+++ test_suite.py
@@ -5,7 +5,7 @@
 
 import numpy as np
 
-from profilers import Warning, ErrorType, Severity
+from profilers import ErrorType, Severity, DataFrameProfiler
 from analyzers import DataScale
 
 
@@ -13,8 +13,46 @@ class Check:
     def __init__(self, assumption, error_type, severity, message):
         self.assumption = assumption
         self.error_type = error_type
-        self.message = message
+        self.message = message.strip()
         self.severity = severity
+
+    def __repr__(self):
+        return """Check:{:s} - IF {:s}""".format(self.error_type.name,
+                                                 self.message)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __hash__(self):
+        return hash((self.error_type.value,
+                     self.message, self.severity.value))
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
+
+
+class Warning:
+    def __init__(self, error_type, severity, message=""):
+        self.error_type = error_type
+        self.message = message.strip()
+        self.severity = severity
+
+    def __repr__(self):
+        return "{:10}: {:s}".format(self.severity.name, self.message)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __hash__(self):
+        return hash((self.error_type.value, self.message,
+                     self.severity.value))
+
+    def __eq__(self, other):
+        # return self.__hash__() == other.__hash__()
+        a = (self.error_type.value == other.error_type.value)
+        b = (self.message == other.message)
+        c = (self.severity.value == other.severity.value)
+        return a and b and c
 
 
 class Test:
@@ -24,6 +62,13 @@ class Test:
 
     def __iter__(self):
         return iter(self.checks)
+
+    def __repr__(self):
+        checks = ", ".join(map(str, self.checks))
+        return """Test:{:10} ({:s})""".format(self.severity.name, checks)
+
+    def __str__(self):
+        return self.__repr__()
 
     def _add(self, assumption, error_type, message, severity=None):
         severity = severity if severity is not None else self.severity
@@ -37,7 +82,7 @@ class Test:
 
     def is_complete(self, column):
         return self._add(lambda x: x[column].notna().all(),
-                         ErrorType.INCOMPLETE, """
+                         ErrorType.MISSING_VALUE, """
                          Column %s is not complete""" % column)
 
     def is_unique(self, column):
@@ -51,30 +96,34 @@ class Test:
                          Values in column %s are not in range %s."""
                          % (column, str(range)))
 
+    def __hash__(self):
+        return hash((self.severity.value, tuple(self.checks)))
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
+
 
 class TestSuite:
     def __init__(self):
-        self.data = None
         self.tests = []
 
     def on(self, data):
-        self.data = data
-        return self
+        return self.run(data)
 
     def add(self, test):
         self.tests.append(test)
         return self
 
-    def run(self):
-        assert self.data is not None, "Call TestSuite on(data) method first."
-        self.warnings = []
+    def run(self, data):
+        assert data is not None, "Call TestSuite on(data) method first."
+        warnings = []
         for test in self.tests:
             for check in test:
-                if check.assumption(self.data):
+                if check.assumption(data):
                     continue
-                self.warnings.append(Warning(check.error_type,
-                                             Severity.CRITICAL, check.message))
-        return self.warnings
+                warnings.append(Warning(check.error_type,
+                                        Severity.CRITICAL, check.message))
+        return warnings
 
 
 class AutomatedTestSuite:
@@ -90,16 +139,17 @@ class AutomatedTestSuite:
                 'missing_values': lambda x: self.missing_values(x)
             })
             self.nominal_col = dict({
-                'values_in_range': lambda x: self.values_in_range(x)
+                'values_in_range': lambda x: Test(
+                    Severity.CRITICAL).is_in_range(x.column_name, x.range)
             })
             self.ordinal_col = dict({
-                'values_in_range': lambda x: self.values_in_range(x)
+
             })
             self.interval_col = dict({
-                'values_in_range': lambda x: self.values_in_range(x)
+
             })
             self.ratio_col = dict({
-                'values_in_range': lambda x: self.values_in_range(x)
+
             })
             self.set_wise = dict({
 
@@ -119,20 +169,9 @@ class AutomatedTestSuite:
             # An error is critical if there weren't any missing values
             # discovered before. If missing values originally present - this
             # issue is considered to be known thus not critical.
-            condition = col_profile.num_missing == 0
-            severity = Severity.CRITICAL if condition else Severity.INFO
-            return (Test(severity).is_complete(col_profile.column_name),
-                    Warning(ErrorType.MISSING_VALUE, severity, """
-                            Column %s contains missing values.
-                            """ % col_profile.column_name))
-
-        def values_in_range(self, col_profile):
-            return (Test(Severity.CRITICAL)
-                    .is_in_range(col_profile.column_name, col_profile.range),
-                    Warning(ErrorType.NOT_IN_RANGE, Severity.CRITICAL, """
-                            Values in column %s are not in range %s.
-                            """ % (col_profile.column_name,
-                                   col_profile.range)))
+            # condition = col_profile.num_missing == 0
+            # severity = Severity.CRITICAL if condition else Severity.INFO
+            return Test(Severity.CRITICAL).is_complete(col_profile.column_name)
 
     instance = None
 
@@ -143,38 +182,35 @@ class AutomatedTestSuite:
     def __getattr__(self, name):
         return getattr(self.instance, name)
 
-    def run(self, data_profile, pipeline_profile):
+    def run(self, data, pipeline_profile):
+        data_profile = DataFrameProfiler().on(data)
         tests, warnings = [], []
         if data_profile is not None:
             for column_profile in data_profile:
-                print(column_profile)
+                # print(column_profile)
                 for k, v in self.col_wise.items():
-                    print(k)
-                    test, warning = v(column_profile)
-                    tests.append(test)
-                    warnings.append(warning)
+                    tests.append(v(column_profile))
                 for k, v in self.scale_wise[column_profile.scale].items():
-                    print(k)
-                    test, warning = v(column_profile)
-                    tests.append(test)
-                    warnings.append(warning)
+                    tests.append(v(column_profile))
             for k, v in self.set_wise.items():
-                print(k)
-                test, warning = v(data_profile)
-                tests.append(test)
-                warnings.append(warning)
+                tests.append(v(data_profile))
         else:
             warnings.append(Warning(ErrorType.UNAVAILABLE, Severity.CRITICAL,
                                     self.mes_unavailable_data_profile))
         if pipeline_profile is not None:
             for step in pipeline_profile:
-                print(step)
+                # print(step)
                 for k, v in self.pipe.items():
-                    print(k)
-                    v()
+                    tests.append(v(pipeline_profile))
         else:
             warnings.append(Warning(ErrorType.UNAVAILABLE, Severity.CRITICAL,
                                     self.mes_unavailable_pipeline_profile))
+        for test in tests:
+            for check in test.checks:
+                if check.assumption(data):
+                    continue
+                warnings.append(
+                    Warning(check.error_type, test.severity, check.message))
         return tests, warnings
 
 
